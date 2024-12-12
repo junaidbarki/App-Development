d9c6b7a21e980d36bfd2b62406a4b91744018185
junaidbarki <junaidbarki412824@gmail.com>
2024-12-12 01:30:52 -0800

    first commmit

--- app/src/main/java/com/example/headache/MainActivity.kt
+++ app/src/main/java/com/example/headache/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Text(
                 text = "Hello Android!",
-             color = Color.Red
+             color = Color.Gray
             )
         }
     }
