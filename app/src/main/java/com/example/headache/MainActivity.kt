ed3342389436b7d4dc72c1eff22eaf9349987f63
junaidbarki <junaidbarki412824@gmail.com>
2024-12-12 01:38:38 -0800

    second commit

--- app/src/main/java/com/example/headache/MainActivity.kt
+++ app/src/main/java/com/example/headache/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Text(
-                text = "Hello Android!",
+                text = "Hello Android with Kotlin!",
              color = Color.Gray
             )
         }
